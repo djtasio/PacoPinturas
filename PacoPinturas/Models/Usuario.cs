d938eb2c36fa2949f5ce18f3dc95d17a50a1779a
Guillermo Sanchez Sanz <guillermomonterde@gmail.com>
2022-11-16 16:44:45 +0100

    esqueleto

---
+++ PacoPinturas/Models/Usuario.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PacoPinturas.Models
+{
+    internal class Usuario
+    {
+        public string Id { get; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string NameSurname { get; set; }
+        public int Phone { get; set; }
+
+        //Listado de pedidos
+
+        private static int idNumberSeed = 1;
+
+        public Usuario()
+        {
+            Id = idNumberSeed.ToString();
+            idNumberSeed++;
+        }
+
+        public Usuario(string username, string password, string nameSurname, int phone)
+        {
+            Id = idNumberSeed.ToString();
+            idNumberSeed++;
+            Username = username;
+            Password = password;
+            NameSurname = nameSurname;
+            Phone = phone;
+            //Listado de pedidos declarado
+        }
+    }
+}
