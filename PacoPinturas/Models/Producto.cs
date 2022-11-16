95ce6226f9363a7b5a07a2df46367e5758fa8618
diego <djtasio2002@gmail.com>
2022-11-16 17:04:09 +0100

    productossssssss

---
+++ PacoPinturas/Models/Producto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PacoPinturas.Models
+{
+    class Producto
+    {
+        public string id { get; set; }
+        public string color { get; set; }
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+        public string descripcion { get; set; }
+        public enum Calidad { Estandar,Premium}
+        public enum Productos { Spray, Cubo, Rotulador}
+
+        private static int idNumberSeed = 1;
+
+        public Producto()
+        {
+
+        }
+
+        public Producto(string color, int cantidad,decimal precio,string descripcion, Calidad calidad , Productos productos){
+            this.id = idNumberSeed.ToString();
+            
+
+        }
+
+    }
+}
