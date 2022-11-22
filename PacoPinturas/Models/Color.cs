18115aab84c1b0cca755f01f9ad90041a6a3803e
Guillermo Sanchez Sanz <guillermomonterde@gmail.com>
2022-11-22 18:16:57 +0100

    Clase color

---
+++ PacoPinturas/Models/Color.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PacoPinturas.Models
+{
+    internal class Color
+    {
+        public string Name { get; set; }
+        public string Code { get; set; }
+
+        public Color() {
+
+        }
+
+        public Color(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+    }
+}
