19190031d00e553aefcf244cad86b8db4bbc3ca6
Guillermo Sanchez Sanz <guillermomonterde@gmail.com>
2022-11-22 18:29:37 +0100

    Json using

--- PacoPinturas/Functions/Metodos.cs
+++ PacoPinturas/Functions/Metodos.cs
@@ -3,6 +3,8 @@ using PacoPinturas.Models;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
+using System.Transactions;
 
 namespace PacoPinturas.Functions
 {
@@ -66,5 +68,14 @@ namespace PacoPinturas.Functions
             var user = users.Find(user => user.User.Equals(username) && user.Contrasenia.Equals(password));
             return user;
         }
+
+        //Leer colores
+        public static string readColors(){
+        {
+                string fileName = $@"{Owner}_{Number}.json";
+                string jsonString = File.ReadAllText(fileName);
+                List<Transaction>? lista = JsonSerializer.Deserialize<List<Transaction>>(jsonString)!;
+                var report = new System.Text.StringBuilder();
+            }
     }
 }
